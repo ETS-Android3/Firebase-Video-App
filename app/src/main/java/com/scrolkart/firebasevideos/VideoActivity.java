bd1d81438e2768a525f592ee2b5dfe43a63d3279
Suraj Jaiswal <surazjaiswal@gmail.com>
2022-02-24 16:56:51 +0530

    Added sample resources

--- app/src/main/java/com/scrolkart/firebasevideos/VideoActivity.java
+++ app/src/main/java/com/scrolkart/firebasevideos/VideoActivity.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 
 public class VideoActivity extends AppCompatActivity {
 
-
     FloatingActionButton btn_add_video;
     ViewPager2 viewPager2;
     ArrayList<MediaObject> mediaObjects;
@@ -31,34 +30,35 @@ public class VideoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // hide status bar
+        getSupportActionBar().hide();
 
         // Action bar Title
-        setTitle("FireBase Videos");
+//        setTitle("FireBase Videos");
 
         viewPager2 = (ViewPager2)findViewById(R.id.viewPager);
         mediaObjects = new ArrayList<>();
 
          // For Data from firebase realtime database
-        /*DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("Videos");
-        ValueEventListener valueEventListener = new ValueEventListener() {
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("Videos");
+        db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 mediaObjects.clear();
                 for(DataSnapshot data : snapshot.getChildren()){
                     MediaObject object = data.getValue(MediaObject.class);
                     mediaObjects.add(object);
-                    System.out.println(object.getTitle());
+                    System.out.println(object);
                 }
+                viewPager2.setAdapter(new VideoAdapter(mediaObjects));
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        };*/
+        });
 
         // Hard coded video example for testing purpose
-        MediaObject object1 = new MediaObject("1","Big Buck Bunny","Sample Description 1","001","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+       /* MediaObject object1 = new MediaObject("1","Big Buck Bunny","Sample Description 1","001","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
         MediaObject object2 = new MediaObject("2","Elephant Dream","Sample Description 2","002","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4");
         MediaObject object3 = new MediaObject("3","For Bigger Blazes","Sample Description 3","003","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4");
         MediaObject object4 = new MediaObject("4","For Bigger Escape","Sample Description 4","004","https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4");
@@ -67,9 +67,9 @@ public class VideoActivity extends AppCompatActivity {
         mediaObjects.add(object2);
         mediaObjects.add(object3);
         mediaObjects.add(object4);
-        mediaObjects.add(object5);
+        mediaObjects.add(object5);*/
 
-        viewPager2.setAdapter(new VideoAdapter(mediaObjects));
+//        viewPager2.setAdapter(new VideoAdapter(mediaObjects));
 
 
         // Upload video button
@@ -82,8 +82,5 @@ public class VideoActivity extends AppCompatActivity {
                 startActivity(new Intent(VideoActivity.this,AddVideoActivity.class));
             }
         });
-
-
-
     }
 }
